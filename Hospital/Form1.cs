ba90aa974b1fcb9195a2cf2af37745335e780b5e
Daniel-Sameh <111789605+Daniel-Sameh@users.noreply.github.com>
2024-05-12 15:52:16 +0300

    Add files via upload

---
+++ Hospital/Form1.cs
@@ -0,0 +1,43 @@
+using System.Data.SqlClient;
+namespace Hospital
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void open_Click(object sender, EventArgs e)
+        {
+            /*string connectString = "Data Source =DESKTOP-P4RUM8H;Initial Catalog=hospital;Integrated Security=true";
+            SqlConnection con= new SqlConnection(connectString);
+            con.Open();
+            string query = "select * from employee";
+            SqlCommand cmd = new SqlCommand(query, con);
+            SqlDataReader rdr = cmd.ExecuteReader();*/
+
+            /*while(rdr.Read())
+            {
+                string output= "Emp: " + rdr.GetValue(0) + ", "+rdr.GetValue(1)+" "+rdr.GetValue(2) + ", "+rdr.GetValue(4) + ", "+ rdr.GetValue(7) + ", ";
+                MessageBox.Show(output);
+            }*/
+
+            login loginForm = new();
+            loginForm.Show();
+
+
+        }
+
+    }
+}
